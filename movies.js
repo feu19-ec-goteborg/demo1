2393e95352cf3ca75e4ed615781dc3baaaf99abd
David Andersson <David Andersson>
2019-10-03 14:30:40 +0200

    Meow

--- movies.js
+++ movies.js
@@ -1,18 +1,1 @@
-const movies = [
-	{
-		title: 'Star Wars',
-		year: 1977
-	},
-	{
-		title: 'Braveheart',
-		year: 1995
-	},
-	{
-		title: 'Inception',
-		year: 2010
-	},
-	{
-		title: 'The Fast and the Furious',
-		year: 2001
-	}
-]
+dsfcgvhbn
